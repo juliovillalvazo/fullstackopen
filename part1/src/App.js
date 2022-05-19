9325edaa9bbb57dcf6fbb0dd44dca7e8455873a8
Julio Villalvazo <juliovillalvazo26@gmail.com>
2022-05-18 21:39:50 -0500

    finished handling events part

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,11 +3,14 @@ import { useState } from 'react';
 const App = () => {
     const [counter, setCounter] = useState(0);
 
+    const increaseByOne = () => setCounter(counter + 1);
+    const setToZero = () => setCounter(0);
+
     return (
         <div>
             {counter}
-            <button onClick={() => setCounter(counter + 1)}>plus</button>
-            <button onClick={() => setCounter(0)}>reset</button>
+            <button onClick={increaseByOne}>plus</button>
+            <button onClick={setToZero}>reset</button>
         </div>
     );
 };
