8f262fb84e37136a4b914cb793b684e08dd1a8c2
Julio Villalvazo <juliovillalvazo26@gmail.com>
2022-05-18 21:46:16 -0500

    passing states to childs finished

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from 'react';
 
+const Display = (props) => {
+    return <div>{props.counter}</div>;
+};
+
+const Button = (props) => {
+    return <button onClick={props.onClick}>{props.text}</button>;
+};
+
 const App = () => {
     const [counter, setCounter] = useState(0);
 
     const increaseByOne = () => setCounter(counter + 1);
     const setToZero = () => setCounter(0);
+    const decreaseByOne = () => setCounter(counter - 1);
 
     return (
         <div>
-            {counter}
-            <button onClick={increaseByOne}>plus</button>
-            <button onClick={setToZero}>reset</button>
+            <Display counter={counter} />
+            <Button text="plus" onClick={increaseByOne} />
+            <Button onClick={setToZero} text="reset" />
+            <Button onClick={decreaseByOne} text="minus" />
         </div>
     );
 };
