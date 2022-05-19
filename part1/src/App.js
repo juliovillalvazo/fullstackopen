fc39316a3a62d7f6c6feef92d2a7d93b143c9d05
Julio Villalvazo <juliovillalvazo26@gmail.com>
2022-05-18 23:58:12 -0500

    finished part 1d

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,49 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return <div>the app is used by pressing the buttons</div>;
+    }
+    return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 const App = () => {
-    const [counter, setCounter] = useState(0);
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([]);
 
-    const increaseByOne = () => setCounter(counter + 1);
-    const setToZero = () => setCounter(0);
-    const decreaseByOne = () => setCounter(counter - 1);
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'));
+        setLeft(left + 1);
+    };
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'));
+        setRight(right + 1);
+    };
+
+    const Button = ({ handleClick, text }) => (
+        <button onClick={handleClick}>{text}</button>
+    );
+    // const [clicks, setClicks] = useState({
+    //     left: 0,
+    //     right: 0,
+    // });
+
+    // const handleLeftClick = () =>
+    //     setClicks({ ...clicks, left: clicks.left + 1 });
+
+    // const handleRightClick = () =>
+    //     setClicks({ ...clicks, right: clicks.right + 1 });
 
     return (
         <div>
-            <Display counter={counter} />
-            <Button text="plus" onClick={increaseByOne} />
-            <Button onClick={setToZero} text="reset" />
-            <Button onClick={decreaseByOne} text="minus" />
+            {left}
+            <Button handleClick={handleLeftClick} text="left" />
+            <Button handleClick={handleRightClick} text="right" />
+            {right}
+            <History allClicks={allClicks} />
         </div>
     );
 };
-
 export default App;
